3fd73334f1f35c50a72612bc478ba855c0bc6d04
chojrak <chacinski.rafal@gmail.com>
2020-07-20 18:25:43 +0200

    git init

---
+++ src/test/java/com/generatorimprez/GEN/GenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generatorimprez.GEN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
